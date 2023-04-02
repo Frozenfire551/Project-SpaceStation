c93727ac7e0e96fad50aa26b02ff435746b6d325
Frozenflame <nikgreen496@gmail.com>
2023-04-02 14:28:52 +1300

    Basic Unreal Added

---
+++ Project_SpaceStation/Source/Project_SpaceStation/Project_SpaceStationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project_SpaceStationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_SPACESTATION_API AProject_SpaceStationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
