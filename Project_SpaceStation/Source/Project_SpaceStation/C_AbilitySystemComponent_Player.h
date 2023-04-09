c755fc682be98b5460124825afa8be9108d6c2c6
Frozenflame <nikgreen496@gmail.com>
2023-04-09 16:12:40 +1200

    basic files created for GAS

---
+++ Project_SpaceStation/Source/Project_SpaceStation/C_AbilitySystemComponent_Player.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "C_AbilitySystemComponent_Player.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_SPACESTATION_API UC_AbilitySystemComponent_Player : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
